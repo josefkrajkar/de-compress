51436b605e2c3112fef00cf0020b00c2ad1eba04
Josef Krajkář <josefkrajkar@Josef--MacBook-Pro.local>
2025-01-03 15:24:22 +0100

    feat: Add lz77 compress algorithm

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,39 @@ import { useState } from 'react';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 
+// Utils
+import { lz77Compress } from './helpers/compressFunctions';
+
 // Styles
 import './App.css';
 
 function App() {
   const [file, setFile] = useState<File | null>(null);
+
+  function processFile() {
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      const text = reader.result;
+      if (typeof text !== 'string') return;
+
+      // Compress
+      const compressed = lz77Compress(text);
+      const compressedString = JSON.stringify(compressed);
+
+      // Create downloadable blob
+      const blob = new Blob([compressedString], { type: 'application/json' });
+      const url = URL.createObjectURL(blob);
+
+      // Download
+      const link = document.getElementById('download-link') as HTMLAnchorElement;
+      link.href = url;
+      link.click();
+    };
+
+    reader.readAsText(file);
+  }
 
   return (
     <>
@@ -27,9 +55,14 @@ function App() {
             onChange={e => setFile(e.target.files?.[0] || null)}
           />
           <button onClick={() => document.querySelector('input')?.click()}>Upload</button>
-          <button disabled={!file} className={!file ? 'disabled' : ''}>
+          <button onClick={processFile} disabled={!file} className={!file ? 'disabled' : ''}>
             Process and download
           </button>
+          {file && (
+            <a id="download-link" className="hidden" href={''} download={'compressed.json'}>
+              Download
+            </a>
+          )}
         </div>
       </main>
 
